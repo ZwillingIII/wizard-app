8f89a732a765fd5da911ded8b1e8b4f3c94ef6c8
zwilling <zwill@bokus.ru>
2024-07-17 17:37:15 +0300

    Добавлены методы получения данных по id о Волшебном мире

--- app/Helpers/Wizard.php
+++ app/Helpers/Wizard.php
@@ -8,6 +8,8 @@ class Wizard
 {
     const WIZARD_HTTP = 'https://wizard-world-api.herokuapp.com';
 
+    private static string $link;
+
     public function __construct()
     {
     }
@@ -25,5 +27,31 @@ class Wizard
     public static function getIngredients()
     {
         return (new Client([]))->request('GET', self::WIZARD_HTTP . '/Ingredients')->getBody()->getContents();
+    }
+
+    public static function elixir(string $id)
+    {
+        self::$link = self::WIZARD_HTTP . '/Elixirs/' . $id;
+
+        return new static();
+    }
+
+    public function get()
+    {
+        return (new Client([]))->request('GET', self::$link)->getBody()->getContents();
+    }
+
+    public static function spell(string $id)
+    {
+        self::$link = self::WIZARD_HTTP . '/Spells/' . $id;
+
+        return new static();
+    }
+
+    public static function ingredient(string $id)
+    {
+        self::$link = self::WIZARD_HTTP . '/Ingredients/' . $id;
+
+        return new static();
     }
 }
